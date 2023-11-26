fa6019ef7aca526105af28d9964d9574e095e455
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2023-11-26 16:28:01 +0100

    styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,63 @@
 "use client";
 import React, { useState } from "react";
 import { observer } from "mobx-react-lite";
-import { AppState, Symbol } from "@/state/AppState";
+import { AppState } from "@/state/AppState";
 import { Select } from "./Select";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import { Password } from "./Password";
+import { ResetCss } from "./ResetCss";
+
+const AppWrapper = styled.div`
+    background: #999;
+    @media (min-width: 1200px) {
+        width: 600px;
+        margin: auto;
+        margin-top: 100px;
+        border-radius: 16px;
+    }
+`;
+
+const TitleWrapper = styled.div`
+    padding: 16px;
+    h1 {
+        font-size: 3em;
+    }
+    h2 {
+        font-size: 1.6em;
+    }
+`;
 
 const SelectWrapper = styled.div`
     display: flex;
     gap: 16px;
     padding: 16px;
 `;
+
+const GlobalStyles = createGlobalStyle`
+    html {
+        font-size: 16px;
+    }
+
+    body {
+        background: #333;
+        font-family: sans-serif;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+`;
+
 export const App = observer(() => {
     const [appState] = useState(() => new AppState());
     return (
-        <>
+        <AppWrapper>
+            <ResetCss />
+            <GlobalStyles />
+            <TitleWrapper>
+                <h1>Lomax</h1>
+                <h2>password generator</h2>
+            </TitleWrapper>
             <SelectWrapper>
                 <Select
                     label="Stage"
@@ -36,6 +79,6 @@ export const App = observer(() => {
                 />
             </SelectWrapper>
             <Password password={appState.password} />
-        </>
+        </AppWrapper>
     );
 });
--- src/components/Symbol.tsx
+++ src/components/Symbol.tsx
@@ -7,12 +7,14 @@ export const SvgBase = styled.svg`
 
 export const SymbolWrapper = styled.div`
     display: flex;
+    justify-content: space-around;
     gap: 8px;
     padding: 8px;
+    width: 100%;
 `;
 
 export const Cross = () => (
-    <SvgBase width="642" height="642" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <SvgBase width="32" height="32" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="321" cy="321" r="321" fill="#333333" />
         <line x1="157.142" y1="156.858" x2="482.954" y2="482.67" stroke="#9BADE4" strokeWidth="40" />
         <line x1="160.858" y1="482.67" x2="486.67" y2="156.858" stroke="#9BADE4" strokeWidth="40" />
@@ -20,21 +22,21 @@ export const Cross = () => (
 );
 
 export const Circle = () => (
-    <SvgBase width="642" height="642" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <SvgBase width="32" height="32" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="321" cy="321" r="321" fill="#333333" />
         <circle cx="321" cy="321" r="188" stroke="#F06E6C" strokeWidth="40" />
     </SvgBase>
 );
 
 export const Triangle = () => (
-    <SvgBase width="642" height="642" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <SvgBase width="32" height="32" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="321" cy="321" r="321" fill="#333333" />
         <path d="M126.508 449.25L317 119.942L507.492 449.25H126.508Z" stroke="#38DEC8" strokeWidth="40" />
     </SvgBase>
 );
 
 export const Square = () => (
-    <SvgBase width="642" height="642" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <SvgBase width="32" height="32" viewBox="0 0 642 642" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="321" cy="321" r="321" fill="#333333" />
         <rect x="159" y="158" width="325" height="325" stroke="#D591BD" strokeWidth="40" />
     </SvgBase>
